6dcfffee6f377412e6bb62c0804287cb92e4868c
Bryan Orozco <bryanorozcoweb@gmail.com>
2021-08-02 19:07:48 -0700

    Update functions.php

--- wp-content/themes/baller-stats/functions.php
+++ wp-content/themes/baller-stats/functions.php
@@ -12,37 +12,37 @@
         remove_menu_page( 'options-general.php' );
         remove_menu_page( 'index.php' );
     }
-	
-	// register stats post type
-	register_post_type('stats',
-		array(
-			'labels'             => array(
-				'name'               => _x('Stats', 'post type general name'),
-				'singular_name'      => _x('Stat', 'post type singular name'),
-				'menu_name'          => _x('Stats', 'admin menu'),
-				'name_admin_bar'     => _x('Stat', 'add new on admin bar'),
-				'add_new'            => _x('Add New', 'stat'),
-				'add_new_item'       => __('Add New Stat'),
-				'new_item'           => __('New Stats'),
-				'edit_item'          => __('Edit Stat'),
-				'view_item'          => __('View Stat'),
-				'all_items'          => __('All Stats'),
-				'search_items'       => __('Search Stats'),
-				'parent_item_colon'  => __('Parent Stats:'),
-				'not_found'          => __('No stats found.'),
-				'not_found_in_trash' => __('No stats found in trash.')
-			),
-			'public'             => true,
-			'publicly_queryable' => true,
-			'show_ui'            => true,
-			'show_in_menu'       => true,
-			'query_var'          => true,
-			'rewrite'            => array('slug' => 'stats'),
-			'capability_type'    => 'post',
-			'has_archive'        => true,
-			'hierarchical'       => true,
-			'menu_position'      => null,
-			'menu_icon'          => 'dashicons-media-spreadsheet',
-			'supports'           => array('title', 'revisions'),
-		)
-	);
+
+    // register stats post type
+    register_post_type('stats',
+        array(
+            'labels'             => array(
+                'name'               => _x('Stats', 'post type general name'),
+                'singular_name'      => _x('Stat', 'post type singular name'),
+                'menu_name'          => _x('Stats', 'admin menu'),
+                'name_admin_bar'     => _x('Stat', 'add new on admin bar'),
+                'add_new'            => _x('Add New', 'stat'),
+                'add_new_item'       => __('Add New Stat'),
+                'new_item'           => __('New Stats'),
+                'edit_item'          => __('Edit Stat'),
+                'view_item'          => __('View Stat'),
+                'all_items'          => __('All Stats'),
+                'search_items'       => __('Search Stats'),
+                'parent_item_colon'  => __('Parent Stats:'),
+                'not_found'          => __('No stats found.'),
+                'not_found_in_trash' => __('No stats found in trash.')
+            ),
+            'public'             => true,
+            'publicly_queryable' => true,
+            'show_ui'            => true,
+            'show_in_menu'       => true,
+            'query_var'          => true,
+            'rewrite'            => array('slug' => 'stats'),
+            'capability_type'    => 'post',
+            'has_archive'        => true,
+            'hierarchical'       => true,
+            'menu_position'      => null,
+            'menu_icon'          => 'dashicons-media-spreadsheet',
+            'supports'           => array('title', 'revisions'),
+        )
+    );
